3e6b336c0aacc7c1f2dc491f589b7394148e4d30
DiMFh <d_fotopoulos80@yahloo.com>
2024-03-09 20:59:15 +0200

    SideBar

---
+++ index1.php
@@ -0,0 +1,133 @@
+<html>
+  <head>
+        <title> 2o Αεροπορικό Κέντρο Ελέγχου </title>
+        <link rel="stylesheet" type="text/css" href="index1.css">
+        <meta charset="UTF-8">
+
+  </head>
+  <body>
+    <nav class="main-menu">
+            <ul>
+                <li>
+                    <a href="https://jbfarrow.com">
+                        <i class="fa fa-home fa-2x"></i>
+                        <span class="nav-text">
+                           Home
+                        </span>
+                    </a>
+                  
+                </li>
+                <li class="has-subnav">
+                    <a href="#">
+                        <i class="fa fa-star fa-2x fa-spin"></i>
+                        <span class="nav-text">
+                            ΔΚΤΗΣ
+                        </span>
+                    </a>
+                    
+                </li>
+                <li class="has-subnav">
+                    <a href="#">
+                       <i class="fa fa-star-half-full fa-2x"></i>
+                        <span class="nav-text">
+                            ΥΔΚΤΗΣ
+                        </span>
+                    </a>
+                    
+                </li>
+                <li class="has-subnav">
+                    <a href="#">
+                       <i class="fa fa-comments fa-2x"></i>
+                        <span class="nav-text">
+                            ΕΠΙΤΕΛΕΙΟ
+                        </span>
+                    </a>
+                   
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-fighter-jet fa-2x"></i>
+                        <span class="nav-text">
+                            ΔΕΕ
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-folder-open fa-2x"></i>
+                        <span class="nav-text">
+                           ΔΥΠ
+                        </span>
+                    </a>
+                </li>
+                <li>
+                   <a href="#">
+                       <i class="fa fa-headphones fa-2x"></i>
+                        <span class="nav-text">
+                            Μ. ΕΠΙΧΕΙΡΗΣΕΩΝ
+                        </span>
+                    </a>
+                </li>
+                <li>
+                   <a href="#">
+                        <i class="fa fa-cogs fa-2x"></i>
+                        <span class="nav-text">
+                            Μ. ΥΠΟΣΤΗΡΙΞΗΣ
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                       <i class="fa fa-shield fa-2x"></i>
+                        <span class="nav-text">
+                            ΣΑΦ
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                       <i class="fa fa-archive fa-2x"></i>
+                        <span class="nav-text">
+                            ΣΕΦ
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                       <i class="fa fa-minus fa-2x"></i>
+                        <span class="nav-text">
+                            ΣΕΕΠ
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                       <i class="fa fa-group fa-2x"></i>
+                        <span class="nav-text">
+                            ΕΝΗΜΕΡΩΣΗ ΠΡΣ
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                       <i class="fa fa-info fa-2x"></i>
+                        <span class="nav-text">
+                            SITE STATUS
+                        </span>
+                    </a>
+                </li>
+            </ul>
+
+            <ul class="logout">
+                <li>
+                   <a href="#">
+                         <i class="fa fa-sign-in fa-2x"></i>
+                        <span class="nav-text">
+                            Log In
+                        </span>
+                    </a>
+                </li>  
+            </ul>
+        </nav>
+  </body>
+    </html>
