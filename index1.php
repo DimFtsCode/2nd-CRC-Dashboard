e98a53edacdc741fc11df84e5fb1e1c20f27fdcd
DiMFh <d_fotopoulos80@yahloo.com>
2024-03-11 00:26:23 +0200

    Header - Footer Bootstrap

--- index1.php
+++ index1.php
@@ -1,133 +1,163 @@
 <html>
-  <head>
+    <head>
         <title> 2o Αεροπορικό Κέντρο Ελέγχου </title>
         <link rel="stylesheet" type="text/css" href="index1.css">
         <meta charset="UTF-8">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 bg-primary text-black text-center py-3 header-content position-relative">
+                    <div class="header-image">
+                        <img src="/images/2ake_emb5.gif" alt="Logo" style="width: 80px; height: auto;">
+                    </div>
+                    <div class="header-text">
+                        <h1 style="font-weight: bold;">2ο ΑΚΕ</h1>
+                        <p style="font-weight: bold;">ΑΕΡΟΠΟΡΙΚΟΣ ΕΛΕΓΧΟΣ ΜΕΣΩΝ ΑΕΡΑΜΥΝΑΣ – ΚΕΝΤΡΟ ΣΥΝΘΕΣΗΣ & ΠΑΡΑΓΩΓΗΣ ΑΕΡΟΠΟΡΙΚΗΣ ΕΙΚΟΝΑΣ</p>
+                    </div>
+                 </div>
+            </div>
+    
+            <div class="row">
+                <div class="col-12 col-md-2  py-3 px-0">
+                    <div  class="main-menu">
+                        <ul>
+                            <li>
+                                <a href="./index.php">
+                                    <i class="fa fa-home fa-2x"></i>
+                                    <span class="nav-text" style="font-weight: bold;">
+                                    Home
+                                    </span>
+                                </a>
+                            
+                            </li>
+                            <li class="has-subnav">
+                                <a href="./pages/dktis_init0.php">
+                                    <i class="fa fa-star fa-2x "></i>
+                                    <span class="nav-text" style="font-weight: bold;">
+                                        ΔΚΤΗΣ
+                                    </span>
+                                </a>
+                                
+                            </li>
+                            <li class="has-subnav">
+                                <a href="./pages/ydktis_init0.php">
+                                <i class="fa fa-star-half-full fa-2x"></i>
+                                    <span class="nav-text" style="font-weight: bold;">
+                                        ΥΔΚΤΗΣ
+                                    </span>
+                                </a>
+                                
+                            </li>
+                            <li class="has-subnav">
+                                <a href="./pages/epit_init0.php">
+                                <i class="fa fa-comments fa-2x"></i>
+                                    <span class="nav-text" style="font-weight: bold;">
+                                        ΕΠΙΤΕΛΕΙΟ
+                                    </span>
+                                </a>
+                            
+                            </li>
+                            <li>
+                                <a href="./pages/dee_init0.php">
+                                    <i class="fa fa-fighter-jet fa-2x"></i>
+                                    <span class="nav-text" style="font-weight: bold;">
+                                        ΔΕΕ
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="./pages/dyp_init0.php">
+                                    <i class="fa fa-folder-open fa-2x"></i>
+                                    <span class="nav-text" style="font-weight: bold;">
+                                    ΔΥΠ
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                            <a href="./pages/me_init0.php">
+                                <i class="fa fa-headphones fa-2x"></i>
+                                    <span class="nav-text" style="font-weight: bold;">
+                                        Μ. ΕΠΙΧΕΙΡΗΣΕΩΝ
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                            <a href="./pages/myp_init0.php">
+                                    <i class="fa fa-cogs fa-2x"></i>
+                                    <span class="nav-text" style="font-weight: bold;">
+                                        Μ. ΥΠΟΣΤΗΡΙΞΗΣ
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="./pages/saf_init0.php">
+                                <i class="fa fa-shield fa-2x"></i>
+                                    <span class="nav-text" style="font-weight: bold;">
+                                        ΣΑΦ
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="./pages/sef_init0.php">
+                                <i class="fa fa-archive fa-2x"></i>
+                                    <span class="nav-text" style="font-weight: bold;">
+                                        ΣΕΦ
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="./pages/seep_init0.php">
+                                <i class="fa fa-minus fa-2x"></i>
+                                    <span class="nav-text" style="font-weight: bold;">
+                                        ΣΕΕΠ
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="./pages/ake_init0.php">
+                                <i class="fa fa-group fa-2x"></i>
+                                    <span class="nav-text" style="font-weight: bold;">
+                                        ΕΝΗΜΕΡΩΣΗ ΠΡΣ
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="./pages/Unit_Status0.php">
+                                <i class="fa fa-info fa-2x"></i>
+                                    <span class="nav-text" style="font-weight: bold;">
+                                        SITE STATUS
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
 
-  </head>
-  <body>
-    <nav class="main-menu">
-            <ul>
-                <li>
-                    <a href="https://jbfarrow.com">
-                        <i class="fa fa-home fa-2x"></i>
-                        <span class="nav-text">
-                           Home
-                        </span>
-                    </a>
-                  
-                </li>
-                <li class="has-subnav">
-                    <a href="#">
-                        <i class="fa fa-star fa-2x fa-spin"></i>
-                        <span class="nav-text">
-                            ΔΚΤΗΣ
-                        </span>
-                    </a>
-                    
-                </li>
-                <li class="has-subnav">
-                    <a href="#">
-                       <i class="fa fa-star-half-full fa-2x"></i>
-                        <span class="nav-text">
-                            ΥΔΚΤΗΣ
-                        </span>
-                    </a>
-                    
-                </li>
-                <li class="has-subnav">
-                    <a href="#">
-                       <i class="fa fa-comments fa-2x"></i>
-                        <span class="nav-text">
-                            ΕΠΙΤΕΛΕΙΟ
-                        </span>
-                    </a>
-                   
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fa fa-fighter-jet fa-2x"></i>
-                        <span class="nav-text">
-                            ΔΕΕ
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fa fa-folder-open fa-2x"></i>
-                        <span class="nav-text">
-                           ΔΥΠ
-                        </span>
-                    </a>
-                </li>
-                <li>
-                   <a href="#">
-                       <i class="fa fa-headphones fa-2x"></i>
-                        <span class="nav-text">
-                            Μ. ΕΠΙΧΕΙΡΗΣΕΩΝ
-                        </span>
-                    </a>
-                </li>
-                <li>
-                   <a href="#">
-                        <i class="fa fa-cogs fa-2x"></i>
-                        <span class="nav-text">
-                            Μ. ΥΠΟΣΤΗΡΙΞΗΣ
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                       <i class="fa fa-shield fa-2x"></i>
-                        <span class="nav-text">
-                            ΣΑΦ
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                       <i class="fa fa-archive fa-2x"></i>
-                        <span class="nav-text">
-                            ΣΕΦ
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                       <i class="fa fa-minus fa-2x"></i>
-                        <span class="nav-text">
-                            ΣΕΕΠ
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                       <i class="fa fa-group fa-2x"></i>
-                        <span class="nav-text">
-                            ΕΝΗΜΕΡΩΣΗ ΠΡΣ
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                       <i class="fa fa-info fa-2x"></i>
-                        <span class="nav-text">
-                            SITE STATUS
-                        </span>
-                    </a>
-                </li>
-            </ul>
-
-            <ul class="logout">
-                <li>
-                   <a href="#">
-                         <i class="fa fa-sign-in fa-2x"></i>
-                        <span class="nav-text">
-                            Log In
-                        </span>
-                    </a>
-                </li>  
-            </ul>
-        </nav>
-  </body>
-    </html>
+                        <ul class="logout">
+                            <li>
+                            <a href="./pages/login.php">
+                                    <i class="fa fa-sign-in fa-2x"></i>
+                                    <span class="nav-text" style="font-weight: bold;">
+                                        Log In
+                                    </span>
+                                </a>
+                            </li>  
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-12 col-md-10">
+                        <p class="text-center" style="font-weight: bold;">Επικοινωνία</p>
+                    </div>
+            </div>
+            <div class="row">
+                <div class="col-12 bg-dark text-white text-center py-3 footer-content">
+                    <!-- <div class="footer-content"> -->
+                        <p >Copyright (c) 2017 znk</p>
+                    <!-- </div> -->
+                </div>
+            </div>
+        </div> <!-- end container -->
+       
+    </body>
+</html>
