bcad98c26fb4991a989d27031708ef98f3389ad8
DiMFh <d_fotopoulos80@yahloo.com>
2024-03-11 23:17:42 +0200

    Carouzel + js script for text

--- index1.php
+++ index1.php
@@ -21,7 +21,7 @@
             </div>
     
             <div class="row">
-                <div class="col-12 col-md-2  py-3 px-0">
+                <div class="col-12 col-lg-2  py-3 px-0">
                     <div  class="main-menu">
                         <ul>
                             <li>
@@ -146,9 +146,45 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-12 col-md-10">
-                        <p class="text-center" style="font-weight: bold;">Επικοινωνία</p>
+                <div class="col-12 col-lg-10  py-3 ">
+                    <div class="container-fluid"git add .>
+                        <div class="row">
+                            <div class="col-6 bg-dark">
+                                <div id="carouselExampleIndicators" class="carousel slide py-3" data-ride="carousel">
+                                    <ol class="carousel-indicators">
+                                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                    </ol>
+                                    <div class="carousel-inner">
+                                        <div class="carousel-item active">
+                                            <img src="/images/2ake_over1.jpg" class="img-thumbnail" style="width: 100%; height: 300px; object-fit: cover;" alt="...">
+                                        </div>
+                                        
+                                        <div class="carousel-item">
+                                            <img src="/images/snow-img1.jpg" class="img-thumbnail" style="width: 100%; height: 300px; object-fit: cover;" alt="...">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src="/images/snow-img2.jpeg" class="img-thumbnail" style="width: 100%; height: 300px; object-fit: cover;" alt="...">
+                                        </div>
+                                    </div>
+                                    <div id="carousel-text" class="text-center mt-2">
+                                        <!-- Το κείμενο θα ενημερώνεται από το JavaScript -->
+                                    </div>
+                                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Next</span>
+                                    </a>
+                                    </div>   
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                </div>
             </div>
             <div class="row">
                 <div class="col-12 bg-dark text-white text-center py-3 footer-content">
@@ -158,6 +194,26 @@
                 </div>
             </div>
         </div> <!-- end container -->
-       
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <script>
+            $(document).ready(function(){
+                var carouselTexts = [
+                    { title: "Γενικά", description: "Το 2ο ΑΚΕ βρίσκεται στην κορυφή του όρους Πάρνηθα, σε υψόμετρο 1413 μέτρα" },
+                    { title: "Ιστορικά Στοιχεία", description: "Η Μονάδα λειτουργεί και επιχειρεί από το 1958, αρχικά ως 3η ΜΚΣΕ, στη συνέχεια ως 2ο ΚΕΠ και από 2019 ως 2ο ΑΚΕ" },
+                    { title: "Αποστολή", description: "Αποστολή του 2ου ΑΚΕ είναι η επιτυχής εκτέλεση των λειτουργιών ενεργού Αεράμυνας εντός της περιοχής ευθύνης του, για την ακύρωση ή τη μείωση της αποτελεσματικότητας της εχθρικής από αέρος δράσης, σε Εθνικό και Συμμαχικό επίπεδο." }
+                ];
+
+                $('#carouselExampleIndicators').on('slid.bs.carousel', function (e) {
+                    var activeIndex = $(e.relatedTarget).index();
+                    $("#carousel-text").html(
+                        "<h5>" + carouselTexts[activeIndex].title + "</h5>" +
+                        "<p>" + carouselTexts[activeIndex].description + "</p>"
+                    );
+                });
+            });
+        </script>
+
     </body>
 </html>
